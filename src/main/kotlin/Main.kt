0837cc3a10e90dd37430a9670bfd5fcc9b9da5a0
Курникова Милана Станиславовна <1899-23@std.pkgh.edu>
2025-09-13 12:39:04 +0300

    №3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
+    println("Введите натуральное число ")
+    var input = readln()
+    var number = input.toInt()
+
+    if (number < 0) {
+        println("Ошибка. Введено не натуральное число")
+        return
+    }
+
+    var binaryNumber = ""
+
+    while (number > 0) {
+        binaryNumber = (number % 2).toString() + binaryNumber
+        number /= 2
+    }
+
+    println("Двоичное представление введенного числа: $binaryNumber")
 
 }
